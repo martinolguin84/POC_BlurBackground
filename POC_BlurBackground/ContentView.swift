6490ee271a581d29b03696882e7a3b01baebaf51
Martin Olguin <mro1237@gmail.com>
2023-08-14 11:55:54 -0600

    Initial Commit

---
+++ POC_BlurBackground/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  POC_BlurBackground
+//
+//  Created by Martin Olguin on 14/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ POC_BlurBackground/POC_BlurBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_BlurBackgroundApp.swift
+//  POC_BlurBackground
+//
+//  Created by Martin Olguin on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct POC_BlurBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
